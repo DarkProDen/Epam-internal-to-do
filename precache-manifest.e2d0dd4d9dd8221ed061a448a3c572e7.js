9f13199b0ec91c0b8f67a9ca504ce17f36b7b58b
Denis Vilkov <denisvilkov666@gmail.com>
2022-02-01 16:27:55 +0300

    Updates

---
+++ precache-manifest.e2d0dd4d9dd8221ed061a448a3c572e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bed23ee2f53dcd72f7d2d96404d1a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1226f0e759e5a93d2354",
+    "url": "./static/css/main.58aa16d9.chunk.css"
+  },
+  {
+    "revision": "60627f068fdca0ea0e52",
+    "url": "./static/js/2.1e78e636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1e78e636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1226f0e759e5a93d2354",
+    "url": "./static/js/main.412b4b28.chunk.js"
+  },
+  {
+    "revision": "46c523c0d6d656549b3f",
+    "url": "./static/js/runtime-main.84878c42.js"
+  }
+]);
